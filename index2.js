576ea0e10f0aed5bb32b6d15336de0fc6a78046a
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-12 23:19:51 +0800

    Create a simple routing

--- index2.js
+++ index2.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 // Blocking synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -25,7 +26,19 @@ const http = require('http');
 ///////////////////////////////
 // SERVER
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server!');
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName == '/overview') {
+    res.end('Hello from the OVERVIEW!');
+  } else if (pathName === '/product') {
+    res.end('Hello from the PRODUCT!');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'Hello World',
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
