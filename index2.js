b9979cafcb9b7e55b042ea3bb0f315848bc95bf8
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-13 00:53:11 +0800

    Parsing variable from URL

--- index2.js
+++ index2.js
@@ -51,22 +51,23 @@ const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  console.log(url.parse(req.url, true));
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // Overview page
-  if (pathName === '/' || pathName == '/overview') {
+  if (pathname === '/' || pathname == '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
     const output = tempOverview.replace('{%PRODUCT_CARDS%', cardsHtml);
     res.end(output);
     // Product page
-  } else if (pathName === '/product') {
-    res.end('Hello from the PRODUCT!');
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     // API
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200, {
       'Content-type': 'application/json',
     });
